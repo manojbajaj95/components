2378b5c34e38c8e513bfc63f8c5025268f040ab5
Manoj Bajaj <manojbajaj95@gmail.com>
2024-02-02 10:42:59 +0000

    feat: first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Yet Another Components Library
+
+A React component library
+
+## Usage
+
+Copy paste the required components into your directory.
+
+## Features
+
+- Radix UI Primitives
+- Tailwind CSS
+- Icons from [Lucide](https://lucide.dev)
+- Dark mode with `next-themes`
+
+## License
+
+Licensed under the [MIT license](./LICENSE.md).
---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Yet Another Component Library",
+  description:
+    "Beautifully designed components built with Radix UI, Framer and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/senor_bajaj",
+    github: "https://github.com/manojbajaj95/components",
+    docs: "https://ui.shadcn.com",
+  },
+}
